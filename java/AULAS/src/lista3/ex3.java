ccffcada5c40d7f0164cdd3f5c76cc67001550a3
Barbara790 <barbara2.dossantos@gmail.com>
2021-05-06 17:20:07 -0300

    alterações nos exercícios 3 e 4

--- java/AULAS/src/lista3/ex3.java
+++ java/AULAS/src/lista3/ex3.java
@@ -8,12 +8,23 @@ public class ex3 {
 	public static void main(String[] args) {
 		Scanner leia= new Scanner(System.in);
 		int idade=0;
+		int menosVinteUm=0;
+		int maisCinquenta=0;
 		
 	
-		while(idade > 0){
-			System.out.println(" Digite a sua idade");
+		while(idade != -99){
+			System.out.println(" Digite a sua idade(Se você não deseja continuar, digite -99:");
 			idade=leia.nextInt();
+			
+			if(idade<21) {
+				++menosVinteUm;
+			}
+			if(idade>=50) {
+				++maisCinquenta;
+			}
 		}
+		System.out.println("O total de pessoas com mais de 50 anos é "+maisCinquenta);
+		System.out.println("O total de pessoas com menos de 21 anos é "+menosVinteUm);
 		
 		leia.close();
 		}
--- java/AULAS/src/lista3/ex4.java
+++ java/AULAS/src/lista3/ex4.java
@@ -17,8 +17,8 @@ o número de pessoas calmas com menos de 18 ano
 public class ex4 {
 	public static void main(String[] args) {
 		Scanner leia= new Scanner(System.in);
-		int total=2;
-		int estadoPsicologico;
+		final int TOTAL=2;
+		int estadoPsicologico=0;
 		int mulheresNervosas=0;
 		int homensAgressivos=0;
 		int nerFMais40=0;
@@ -29,19 +29,21 @@ public class ex4 {
 		int informacoes=0;
 		int pessoasNervosas=0;
 		int outrosCalmos=0;
+		int pessoasAgressivas=0;
 		
-		while(informacoes<total) {
+		while(informacoes<TOTAL) {
 			informacoes++;
 			
+			System.out.println("Pessoa "+informacoes);
 			System.out.println("Digite a sua idade:");
 			idade=leia.nextInt();
 			System.out.println("Digite o seu genero(1-feminino / 2-masculino / 3-Outros):");
 			genero=leia.nextInt();
 			System.out.println("Digite como você se sente(1-calme / 2-nervose / 3-agressive):"); 
 			estadoPsicologico=leia.nextInt();
-			
 			if(estadoPsicologico==1) {
-				if(idade<19) {
+				pessoasCalmas++;
+				if(pessoasCalmas<19) {
 					calFMenos18++;		
 				}
 				if(genero==3) {
@@ -49,26 +51,30 @@ public class ex4 {
 				}
 			}
 			if(estadoPsicologico==2) {
-				if(idade>39) {
-					nerFMais40++;
+				pessoasNervosas++;
+				if(pessoasNervosas>39) {
+					pessoasNervosas++;
 				}
 				if(genero==1) {
 					mulheresNervosas++;
 				}
 			}
-			if(estadoPsicologico==3 && genero==3) {
+			if(estadoPsicologico==3 && genero==2) {
 				homensAgressivos++;
-				
 			}
 			
 			
-			}
+		}
+			
+			
+			
+			
 		System.out.println("O número de mulheres nervosas é: "+mulheresNervosas);
 		System.out.println("O número de homens agressivos é: "+homensAgressivos);
 		System.out.println("O número de outros calmos é: "+outrosCalmos);
 		System.out.println("O número de pessoas calmas é: "+pessoasCalmas);
 		System.out.println("O número de pessoas calmas com menos de 18 anos é: "+calFMenos18);
-		System.out.println("O número de pessoas nervosas com mais de 40 anos é: "+nerFMais40);
+		System.out.println("O número de pessoas nervosas com mais de 40 anos é: "+pessoasNervosas);
 
 			
 	
