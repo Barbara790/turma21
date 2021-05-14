d01cfbe1edf30a77e77da9ab766225e9fa9651bf
Barbara790 <barbara2.dossantos@gmail.com>
2021-05-14 20:04:20 -0300

    Projeto GIRL POWER

--- java/AULAS/src/exemplos/ProgramaOver.java
+++ java/AULAS/src/exemplos/ProgramaOver.java
@@ -1,12 +1,30 @@
 package exemplos;
 
+import java.util.Scanner;
+
 public class ProgramaOver {
 	public static void main(String[] args) {
-		ExemploOver funcionario=new ExemploOver("177774","João",6,14);
-		ExemploOver2 fun2=new ExemploOver2("849289","jo",7,11,14);
+		Scanner leia=new Scanner(System.in);
+		ExemploOver2 terceiro=new ExemploOver2("849289","jo",7,11,14);
+	
 		
+		System.out.println("Digite o nome:");
+		String nome=leia.next();
+		System.out.println("Digite a matrícula:");
+		String matricula=leia.next();
+		System.out.println("Digite o número de horas trabalhas:");
+		int horasTrabalhadas=leia.nextInt();
+		System.out.println("Digite o valor da hora:");
+		double valorSalario=leia.nextDouble();
+		
+		
+		ExemploOver funcionario=new ExemploOver(matricula,nome,horasTrabalhadas,valorSalario);
+		
+		System.out.println(funcionario.getNome());
 		funcionario.mostrarSalario();
-		fun2.mostrarSalario();
+		
+		System.out.println(terceiro.getNome());
+		terceiro.mostrarSalario();
 	
 	}
 
